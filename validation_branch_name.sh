3094c57a2bc56e3fe730bb639b04b9ec05431e9d
Artur B <103141844+ArturBed@users.noreply.github.com>
2022-04-15 20:40:25 +0200

    Create validation_branch_name.sh

---
+++ validation_branch_name.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo -e " Welcome _o/ \n"
+
+function Run(){
+if [[ $boolean = FALSE ]]; then
+if [[ $string != "main" && $choice = "PRODUCTION" ]]; then
+echo "nooo :("
+fi
+else 
+echo "_o/ validation omitted \o_"
+fi
+}
+
+# _o/
+Run
